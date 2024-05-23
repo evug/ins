b14fe8a8e79edd07e6546f1f889545710cb9a779
System Engineer <ckopny@gmail.com>
2024-05-23 13:16:55 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -82,17 +82,39 @@ function install_firefox() { #HELP Install Firefox:\nBOCKER firefox
   sudo apt install firefox
 }
 function install_gcpsdk() { #HELP Install GCP SDK:\nBOCKER gcpsdk
-  echo "deb [arch=$arch signed-by=/usr/share/keyrings/cloud.google.gpg ] https://packages.cloud.google.com/apt cloud-sdk main" \
-  | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-  if [ "$(lsb_release -r | tr -d '.' | cut -f2)" -le  "2110" ]
-    then
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \
-    | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-    else 
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \
-    | sudo tee /usr/share/keyrings/cloud.google.gpg
-  fi
+  sudo apt-get update
+  sudo apt-get install apt-transport-https ca-certificates gnupg curl
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \
+    | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
+    | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
   update_sourcelist
+  echo 'google-cloud-cli
+google-cloud-cli-anthos-auth
+google-cloud-cli-app-engine-go
+google-cloud-cli-app-engine-grpc
+google-cloud-cli-app-engine-java
+google-cloud-cli-app-engine-python
+google-cloud-cli-app-engine-python-extras
+google-cloud-cli-bigtable-emulator
+google-cloud-cli-cbt
+google-cloud-cli-cloud-build-local
+google-cloud-cli-cloud-run-proxy
+google-cloud-cli-config-connector
+google-cloud-cli-datastore-emulator
+google-cloud-cli-firestore-emulator
+google-cloud-cli-gke-gcloud-auth-plugin
+google-cloud-cli-kpt
+google-cloud-cli-kubectl-oidc
+google-cloud-cli-local-extract
+google-cloud-cli-minikube
+google-cloud-cli-nomos
+google-cloud-cli-pubsub-emulator
+google-cloud-cli-skaffold
+google-cloud-cli-spanner-emulator
+google-cloud-cli-terraform-validator
+google-cloud-cli-tests
+kubectl'
   sudo apt install google-cloud-cli kubeclt
   sudo sh -c 'kubectl completion bash >/etc/bash_completion.d/kubectl'
   if [ ! grep -q __start_kubectl ~/.bashrc ];then
