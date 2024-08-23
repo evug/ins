657c45fe521575e74d565d24ba7bb5998f039cba
System Engineer <ckopny@gmail.com>
2024-08-23 15:59:06 +0200

    Update k8s-jammy.sh

--- k8s-jammy.sh
+++ k8s-jammy.sh
@@ -4,10 +4,13 @@ if [ "$(lsb_release -cs)" != "jammy" ]; then
   echo "This script is only for Ubuntu 22.10"; exit 1
 fi
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl git jq
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" \
-| sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg jq
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+
 sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/kubernetes.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 
 version=$(apt-cache policy kubeadm | grep Candidate | awk '{print $2}')
