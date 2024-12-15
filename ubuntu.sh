a74af53565113836c806964169dea83b3b4ef9a6
System Engineer <ckopny@gmail.com>
2024-12-15 18:44:16 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -84,9 +84,8 @@ function install_firefox() { #HELP Install Firefox:\nBOCKER firefox
   sudo apt install firefox
 }
 function install_flutter() { #HELP Install Flutter:\nBOCKER flutter
-  sudo apt update
-  sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
-  sudo apt-get install -y curl git unzip xz-utils zip libglu1-mesa
+  sudo apt install curl git unzip xz-utils zip libglu1-mesa
+  sudo apt install libc6:amd64 libstdc++6:amd64 lib32z1 libbz2-1.0:amd64
   echo "Installing Flutter SDK"
   mkdir "$HOME/.android"
   wget 'https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.27.0-stable.tar.xz' -qP ~/Downloads/
