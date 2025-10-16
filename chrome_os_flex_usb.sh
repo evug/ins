f34fc5795c5d59898e57cbac969b9de03564c493
System Engineer <ckopny@gmail.com>
2025-10-16 23:06:18 +0200

    Refactor checksum validation and USB writing logic

--- chrome_os_flex_usb.sh
+++ chrome_os_flex_usb.sh
@@ -9,29 +9,24 @@ url=$(cat cloudready_recovery.json | jq -r ".[].url" )
 file=$(basename "$url")
 wget "$url"
 
-
-if [ "$(md5sum "$file" | awk '{print $1}')" != "$(cat cloudready_recovery.json | jq -r '.[].md5')" ]; then
+echo "Expected MD5: $(cat cloudready_recovery.json | jq -r '.[].md5')"
+if [ "$(md5sum $file | awk '{print $1}')" != "$(cat cloudready_recovery.json | jq -r '.[].md5')" ]; then
     echo "MD5 Checksum DOES NOT match for $file. WARNING: File may be corrupted or tampered with!"
     exit
 fi
 echo "MD5 Checksum matches for $file. Integrity OK."
 
-EXPECTED_SHA1=$(cat cloudready_recovery.json | jq -r ".[].sha1")
-echo "Expected SHA-1: $EXPECTED_SHA1"
-
-if [ "$(sha1sum "$file" | awk '{print $1}')" != "$(cat cloudready_recovery.json | jq -r '.[].sha1')" ]; then
+echo "Expected SHA-1: $(cat cloudready_recovery.json | jq -r '.[].sha1')"
+if [ "$(sha1sum $file | awk '{print $1}')" != "$(cat cloudready_recovery.json | jq -r '.[].sha1')" ]; then
     echo "SHA-1 Checksum DOES NOT match for $file. WARNING: File may be corrupted or tampered with!"
     exit
 fi
 echo "SHA-1 Checksum matches for $file. Integrity OK."
 
 unzip "$file"
-rm "$file"
 
-if [ "$(md5sum "$file" | awk '{print $1}')" != "$(cat cloudready_recovery.json | jq -r '.[].md5')" ]; then
-    echo "MD5 Checksum DOES NOT match for $file. WARNING: File may be corrupted or tampered with!"
-    exit
-fi
+usb_drive=$(lsblk -o PATH,SERIAL | grep 4C530001081102122170 | cut -f1 -d" ")
+sudo dd if="${file%.zip}" of="$usb_drive" bs=4M status=progress
 
-USB_PRESENT=$(lsblk -o PATH,SERIAL | grep 4C530001081102122170 | cut -f1 -d" ")
-sudo dd if="${file%.zip}" of="$USB_PRESENT" bs=4M status=progress
+rm "$file" cloudready_recovery.json "${file%.zip}"
+
