4c201c5d5bbf128c2a41e99dc1b17515f5358c80
System Engineer <ckopny@gmail.com>
2025-11-23 00:03:10 +0100

    Refactor GitHub CLI installation method in ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -170,13 +170,15 @@ echo "Gemini $(gemini --version) installed"
 }
 
 function install_gh() { #HELP Install GitHub Cli:\nBOCKER gh
-  type -p curl >/dev/null || sudo apt install curl -y
-  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-  && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-  && echo "deb [arch=$arch signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main"\
-  | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-  update_sourcelist
-  sudo apt install gh
+  (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
 }
 
 function install_golang() { #HELP Install Golang:\nBOCKER golang
